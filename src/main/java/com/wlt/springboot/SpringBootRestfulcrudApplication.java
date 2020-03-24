d7e7ef3cc929e57365c70ae8ddf10a8a7f3610ac
t56 <helloforworld@163.com>
2020-03-24 23:09:58 +0800

    测试

--- src/main/java/com/wlt/springboot/SpringBootRestfulcrudApplication.java
+++ src/main/java/com/wlt/springboot/SpringBootRestfulcrudApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestfulcrudApplication {
 
     public static void main(String[] args) {
-        //注释一次啊
         SpringApplication.run(SpringBootRestfulcrudApplication.class, args);
     }
 
