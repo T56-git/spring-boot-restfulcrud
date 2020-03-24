b0aa47afebd2f8ac1dbef94eb08a5fc5f4f62546
t56 <helloforworld@163.com>
2020-03-24 23:13:20 +0800

    测试

--- src/main/java/com/wlt/springboot/SpringBootRestfulcrudApplication.java
+++ src/main/java/com/wlt/springboot/SpringBootRestfulcrudApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestfulcrudApplication {
 
     public static void main(String[] args) {
+        //测试git
         SpringApplication.run(SpringBootRestfulcrudApplication.class, args);
     }
 
