56ef3a5787785b01618e0ed4fda9634fc77052b0
t56 <helloforworld@163.com>
2020-03-26 22:48:30 +0800

    springtask 测试

--- src/main/java/com/wlt/springboot/quartz/TestQuartz1.java
+++ src/main/java/com/wlt/springboot/quartz/TestQuartz1.java
@@ -21,7 +21,6 @@ public class TestQuartz1 {
         log.info("task1:执行 {}" ,dateFormat.format(new Date()));
     }
 
-
     @Scheduled(cron="0 */1 * * * ?")
     public void task2(){
         log.info("===========task2:执行 {}" ,dateFormat.format(new Date()));
